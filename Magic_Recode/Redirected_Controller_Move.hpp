956b6b1292e9a668cf592dbd255f1f80e73f9ca2
User1 <User1@DESKTOP-MV9U5LK>
2020-05-04 13:52:12 +0300

    Reset Mouse

--- Magic_Recode/Redirected_Controller_Move.hpp
+++ Magic_Recode/Redirected_Controller_Move.hpp
@@ -1,21 +1,34 @@
 #pragma once
 
-void __fastcall Redirected_Controller_Move(void* Unknown_Parameter_1, void* Unknown_Parameter_2, void* Unknown_Parameter_3, void* Unknown_Parameter_4)
+void __fastcall Redirected_Controller_Move(void* Input, void* Unknown_Parameter_1, void* Unknown_Parameter_2, void* Unknown_Parameter_3)
 {
+	static __int8 Previous_Draw_Graphical_User_Interface = Draw_Graphical_User_Interface;
+
 	if (Draw_Graphical_User_Interface == 0)
 	{
+		if (Previous_Draw_Graphical_User_Interface == 1)
+		{
+			Previous_Draw_Graphical_User_Interface = 0;
+
+			using Reset_Mouse_Type = void(__thiscall*)(void* Input);
+
+			static void* Reset_Mouse_Location = (void*)((unsigned __int32)GetModuleHandleW(L"client.dll") + 1349472);
+
+			Reset_Mouse_Type((unsigned __int32)Reset_Mouse_Location)(Input);
+		}
+
 		auto Controller_Move = [&]() -> void
 		{
-			using Controller_Move_Type = void(__thiscall*)(void* Unknown_Parameter_1, void* Unknown_Parameter_2, void* Unknown_Parameter_3);
+			using Controller_Move_Type = void(__thiscall*)(void* Input, void* Unknown_Parameter_1, void* Unknown_Parameter_2);
 
-			Controller_Move_Type((unsigned __int32)Original_Controller_Move_Caller_Location)(Unknown_Parameter_1, Unknown_Parameter_3, Unknown_Parameter_4);
+			Controller_Move_Type((unsigned __int32)Original_Controller_Move_Caller_Location)(Input, Unknown_Parameter_2, Unknown_Parameter_3);
 		};
 
 		if (Freeze_Controlled_Creature == 2)
 		{
 			Freeze_Controlled_Creature = 0;
 
-			*(float*)((unsigned __int32)Unknown_Parameter_1 + 5) = 0;
+			*(float*)((unsigned __int32)Input + 5) = 0;
 
 			Controller_Move();
 		}
@@ -26,5 +39,9 @@ void __fastcall Redirected_Controller_Move(void* Unknown_Parameter_1, void* Unkn
 				Controller_Move();
 			}
 		}
+	}
+	else
+	{
+		Previous_Draw_Graphical_User_Interface = 1;
 	}
 }
