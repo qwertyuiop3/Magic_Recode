eed67c13c3ddc886d29e5e1ca247ca91833388bd
User1 <User1@DESKTOP-MV9U5LK>
2020-05-04 14:29:34 +0300

    Commit

--- Magic_Recode/Redirected_Controller_Move.hpp
+++ Magic_Recode/Redirected_Controller_Move.hpp
@@ -14,27 +14,22 @@ void __fastcall Redirected_Controller_Move(void* Input, void* Unknown_Parameter_
 
 			*(float*)Mouse_Enable_Location = 0;
 		}
-
-		auto Controller_Move = [&]() -> void
-		{
-			using Controller_Move_Type = void(__thiscall*)(void* Input, void* Unknown_Parameter_1, void* Unknown_Parameter_2);
-
-			Controller_Move_Type((unsigned __int32)Original_Controller_Move_Caller_Location)(Input, Unknown_Parameter_2, Unknown_Parameter_3);
-		};
-
-		if (Freeze_Controlled_Creature == 2)
-		{
-			Freeze_Controlled_Creature = 0;
-
-			*(float*)Mouse_Enable_Location = 0;
-
-			Controller_Move();
-		}
 		else
 		{
-			if (Freeze_Controlled_Creature == 0)
+			if (Freeze_Controlled_Creature == 2)
 			{
-				Controller_Move();
+				Freeze_Controlled_Creature = 0;
+
+				*(float*)Mouse_Enable_Location = 0;
+			}
+			else
+			{
+				if (Freeze_Controlled_Creature == 0)
+				{
+					using Controller_Move_Type = void(__thiscall*)(void* Input, void* Unknown_Parameter_1, void* Unknown_Parameter_2);
+
+					Controller_Move_Type((unsigned __int32)Original_Controller_Move_Caller_Location)(Input, Unknown_Parameter_2, Unknown_Parameter_3);
+				}
 			}
 		}
 	}
