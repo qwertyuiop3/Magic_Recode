55354afe5f622c003f88ff11ddd3889eb3aba010
User1 <User1@DESKTOP-MV9U5LK>
2020-06-16 18:26:38 +0300

    Commit

--- Magic_Recode/Redirected_Present.hpp
+++ Magic_Recode/Redirected_Present.hpp
@@ -36,9 +36,9 @@ __int32 __stdcall Redirected_Present(IDirect3DDevice9* Direct_3_Dimensional_Devi
 							{
 								if (Y <= Screen_Height)
 								{
-									On_Screen_Location[0] = X;
+									On_Screen_Location[0] = roundf(X);
 
-									On_Screen_Location[1] = Y;
+									On_Screen_Location[1] = roundf(Y);
 
 									return 1;
 								}
