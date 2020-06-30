02a2a141ce27b25c3c6728330fef491957591396
User1 <User1@DESKTOP-MV9U5LK>
2020-06-30 17:14:47 +0300

    Style Correction

--- Magic_Recode/I_Am_Graphical_User_Interface_Initialize_I_Am_Graphical_User_Interface.hpp
+++ Magic_Recode/I_Am_Graphical_User_Interface_Initialize_I_Am_Graphical_User_Interface.hpp
@@ -6,9 +6,7 @@ void Initialize_I_Am_Graphical_User_Interface(HWND Game_Window_Handle)
 
 	ImGui::StyleColorsDark();
 
-	IDirect3DDevice9* Direct_3_Dimensional_Device_9 = *(IDirect3DDevice9**)((unsigned __int32)GetModuleHandleW(L"shaderapidx9.dll") + 1599884);
-
-	ImGui_ImplDX9_Init(Direct_3_Dimensional_Device_9);
+	ImGui_ImplDX9_Init(*(IDirect3DDevice9**)((unsigned __int32)GetModuleHandleW(L"shaderapidx9.dll") + 1599884));
 
 	ImGui_ImplWin32_Init(Game_Window_Handle);
 }
