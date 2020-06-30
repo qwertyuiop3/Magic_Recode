6e7e98cdde6d7ce933368965465576f92e0e3a98
User1 <User1@DESKTOP-MV9U5LK>
2020-06-30 08:41:33 +0300

    Commit

--- Magic_Recode/Redirected_Physics_Simulate.hpp
+++ Magic_Recode/Redirected_Physics_Simulate.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-void __fastcall Redirected_Physics_Simulate(void* Creature, void* Unknown_Parameter_2)
+void __fastcall Redirected_Physics_Simulate(void* Creature, void* Unknown_Parameter)
 {
 	if (Menu_Select::Freeze_Controlled_Creature == 1)
 	{
 		*(__int8*)((unsigned __int32)Creature + 4552) = 0;
 	}
 
-	using Physics_Simulate_Type = void(__thiscall*)(void* Unknown_Parameter);
+	using Physics_Simulate_Type = void(__thiscall*)(void* Creature);
 
 	Physics_Simulate_Type((unsigned __int32)Original_Physics_Simulate_Caller_Location)(Creature);
 }
