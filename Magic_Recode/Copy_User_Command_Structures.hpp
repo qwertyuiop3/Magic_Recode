bd4da7a36e898851d1d591ba7b78457f2f8bae7c
User1 <User1@DESKTOP-MV9U5LK>
2020-07-05 13:40:09 +0300

    style correction

--- Magic_Recode/Copy_User_Command_Structures.hpp
+++ Magic_Recode/Copy_User_Command_Structures.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "Copy_User_Command_User_Command.hpp"
+#include "Copy_User_Command_User_Command_Structure.hpp"
 
-#include "Copy_User_Command_Compressed_User_Command.hpp"
+#include "Copy_User_Command_Compressed_User_Command_Structure.hpp"
 
-#include "Copy_User_Command_Route.hpp"
+#include "Copy_User_Command_Route_Structure.hpp"
