e3a3016c3eb972b7fdaacb843f61b227cd795a0a
User1 <User1@DESKTOP-MV9U5LK>
2020-07-09 12:28:26 +0300

    Commit

--- Magic_Recode/Write_User_Command_Delta_To_Buffer_Redirect_Write_User_Command_Delta_To_Buffer.hpp
+++ Magic_Recode/Write_User_Command_Delta_To_Buffer_Redirect_Write_User_Command_Delta_To_Buffer.hpp
@@ -2,5 +2,20 @@
 
 void Redirect_Write_User_Command_Delta_To_Buffer(void* Client_Module_Location)
 {
-	Redirection_Manager::Redirect_Function(Original_Write_User_Command_Delta_To_Buffer_Caller_Location, 0, (void*)((unsigned __int32)Client_Module_Location + 0x2DC380), Redirected_Write_User_Command_Delta_To_Buffer);
+	unsigned __int8 Write_user_Command_Delta_To_Buffer_Bytes[6] =
+	{
+		104,
+		
+		83,
+		
+		86,
+		
+		139,
+		
+		217,
+		
+		199
+	};
+
+	Redirection_Manager::Redirect_Function(Original_Write_User_Command_Delta_To_Buffer_Caller_Location, 0, (void*)((unsigned __int32)Byte_Manager::Find_Bytes(sizeof Write_user_Command_Delta_To_Buffer_Bytes, Client_Module_Location, Write_user_Command_Delta_To_Buffer_Bytes, 0) - 10), Redirected_Write_User_Command_Delta_To_Buffer);
 }
