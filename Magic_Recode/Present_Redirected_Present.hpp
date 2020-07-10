4e4298994db8194fcfa8f99b2b368239bd31424b
User1 <User1@DESKTOP-MV9U5LK>
2020-07-10 21:31:26 +0300

    csgo port (wip)

--- Magic_Recode/Present_Redirected_Present.hpp
+++ Magic_Recode/Present_Redirected_Present.hpp
@@ -8,7 +8,32 @@ unsigned __int32 __stdcall Redirected_Present(IDirect3DDevice9* Direct_3_Dimensi
 
 		if (Recorded_Route_Elements_Amount > Visuals_Recorded_Route_Step_Length)
 		{
-			static float* View_Matrix_Location = (float*)((unsigned __int32)GetModuleHandleW(L"engine.dll") + 5954552);
+			static float* View_Matrix_Location = nullptr;
+
+			if (View_Matrix_Location == nullptr)
+			{
+				if (Menu_Select::Game_Identifier == 0)
+				{
+					View_Matrix_Location = (float*)((unsigned __int32)GetModuleHandleW(L"engine.dll") + 5954552);
+				}
+				else
+				{
+					static unsigned __int8 View_Matrix_Bytes[5] =
+					{
+						80,
+
+						15,
+
+						17,
+
+						133,
+
+						120
+					};
+
+					View_Matrix_Location = (float*)(*(unsigned __int32*)((unsigned __int32)Byte_Manager::Find_Bytes(sizeof View_Matrix_Bytes, GetModuleHandleW(L"client.dll"), View_Matrix_Bytes, 0) - 8) + 128);
+				}
+			}
 
 			D3DVIEWPORT9 Direct_3_Dimensional_Viewport_9;
 
@@ -191,6 +216,89 @@ unsigned __int32 __stdcall Redirected_Present(IDirect3DDevice9* Direct_3_Dimensi
 
 			Direct_3_Dimensional_Device_9->SetPixelShader(Previous_Direct_3_Dimensional_Pixel_Shader_9);
 		}
+	}
+
+	//test
+	{
+		Direct_3_Dimensional_Device_9->SetRenderState(D3DRS_SRGBWRITEENABLE, 0);
+
+		unsigned long __int32 Previous_Direct_3_Dimensional_Render_State_Destination_Blend;
+
+		Direct_3_Dimensional_Device_9->GetRenderState(D3DRS_DESTBLEND, &Previous_Direct_3_Dimensional_Render_State_Destination_Blend);
+
+		Direct_3_Dimensional_Device_9->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+
+		Direct_3_Dimensional_Device_9->SetTexture(0, nullptr);
+
+		Direct_3_Dimensional_Device_9->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
+
+		Direct_3_Dimensional_Device_9->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_DIFFUSE);
+
+		Direct_3_Dimensional_Device_9->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_TEXTURE);
+
+		IDirect3DVertexDeclaration9* Previous_Direct_3_Dimensional_Vertex_Declaration_9;
+
+		Direct_3_Dimensional_Device_9->GetVertexDeclaration(&Previous_Direct_3_Dimensional_Vertex_Declaration_9);
+
+		Direct_3_Dimensional_Device_9->SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
+
+		IDirect3DPixelShader9* Previous_Direct_3_Dimensional_Pixel_Shader_9;
+
+		Direct_3_Dimensional_Device_9->GetPixelShader(&Previous_Direct_3_Dimensional_Pixel_Shader_9);
+
+		Direct_3_Dimensional_Device_9->SetPixelShader(nullptr);
+
+		{
+			struct Vertex_Structure
+			{
+				float X;
+
+				float Y;
+
+				float Z;
+
+				float Reciprocal_Homogeneous_W;
+
+				unsigned __int32 Color;
+			};
+
+			Vertex_Structure Vertices[2]
+			{
+				{
+					0,
+
+					0,
+
+					0,
+
+					0,
+
+					D3DCOLOR_ARGB(255, 255, 255, 255)
+				},
+
+				{
+					0,
+
+					100,
+
+					0,
+
+					0,
+
+					D3DCOLOR_ARGB(255, 255, 255, 255)
+				}
+			};
+
+			Direct_3_Dimensional_Device_9->DrawPrimitiveUP(D3DPT_LINELIST, 1, Vertices, sizeof Vertex_Structure);
+		}
+
+		Direct_3_Dimensional_Device_9->SetRenderState(D3DRS_DESTBLEND, Previous_Direct_3_Dimensional_Render_State_Destination_Blend);
+
+		Direct_3_Dimensional_Device_9->SetRenderState(D3DRS_SRGBWRITEENABLE, 1);
+
+		Direct_3_Dimensional_Device_9->SetVertexDeclaration(Previous_Direct_3_Dimensional_Vertex_Declaration_9);
+
+		Direct_3_Dimensional_Device_9->SetPixelShader(Previous_Direct_3_Dimensional_Pixel_Shader_9);
 	}
 
 	if (Controller_Move::Draw_Graphical_User_Interface == 1)
