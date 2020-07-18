eff3c124b1b41fb7c1cbf294cedcd4ec58c53edf
Explorer <Explorer>
2020-07-18 18:46:18 +0300

    Commit

--- Magic_Recode/Header++/Present/Functions/Present_Redirect_Present.hpp
+++ Magic_Recode/Header++/Present/Functions/Present_Redirect_Present.hpp
@@ -4,13 +4,13 @@ void Redirect_Present(IDirect3DDevice9* Direct_3_Dimensional_Device_9)
 {
 	void* Present_Location = *(void**)(*(unsigned __int32*)Direct_3_Dimensional_Device_9 + 68);
 
-	Find_Final_Present_Location_Label:
+	Resolve_Present_Location_Label:
 	{
 		if (*(unsigned __int8*)Present_Location == 232)
 		{
 			Present_Location = (void*)((unsigned __int32)Present_Location + *(unsigned __int32*)((unsigned __int32)Present_Location + 1) + 5);
 
-			goto Find_Final_Present_Location_Label;
+			goto Resolve_Present_Location_Label;
 		}
 		else
 		{
@@ -18,7 +18,7 @@ void Redirect_Present(IDirect3DDevice9* Direct_3_Dimensional_Device_9)
 			{
 				Present_Location = (void*)((unsigned __int32)Present_Location + *(unsigned __int32*)((unsigned __int32)Present_Location + 1) + 5);
 
-				goto Find_Final_Present_Location_Label;
+				goto Resolve_Present_Location_Label;
 			}
 		}
 	}
