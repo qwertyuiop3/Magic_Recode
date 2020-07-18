dcb4a35bd0fda02f4991c3498f9e545b59d19398
Explorer <Explorer>
2020-07-18 18:44:35 +0300

    jmp resolver (gameoverlay)

--- Magic_Recode/Header++/Present/Functions/Present_Redirect_Present.hpp
+++ Magic_Recode/Header++/Present/Functions/Present_Redirect_Present.hpp
@@ -2,5 +2,26 @@
 
 void Redirect_Present(IDirect3DDevice9* Direct_3_Dimensional_Device_9)
 {
-	Redirection_Manager::Redirect_Function(Original_Present_Caller_Location, 0, *(void**)(*(unsigned __int32*)Direct_3_Dimensional_Device_9 + 68), Redirected_Present);
+	void* Present_Location = *(void**)(*(unsigned __int32*)Direct_3_Dimensional_Device_9 + 68);
+
+	Find_Final_Present_Location_Label:
+	{
+		if (*(unsigned __int8*)Present_Location == 232)
+		{
+			Present_Location = (void*)((unsigned __int32)Present_Location + *(unsigned __int32*)((unsigned __int32)Present_Location + 1) + 5);
+
+			goto Find_Final_Present_Location_Label;
+		}
+		else
+		{
+			if (*(unsigned __int8*)Present_Location == 233)
+			{
+				Present_Location = (void*)((unsigned __int32)Present_Location + *(unsigned __int32*)((unsigned __int32)Present_Location + 1) + 5);
+
+				goto Find_Final_Present_Location_Label;
+			}
+		}
+	}
+
+	Redirection_Manager::Redirect_Function(Original_Present_Caller_Location, 0, Present_Location, Redirected_Present);
 }
