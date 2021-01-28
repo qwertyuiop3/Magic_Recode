a34f7e7152ca492e282479d79b2d891363a7a9de
explorer <43570227+qwertyuiop3@users.noreply.github.com>
2021-01-28 05:17:21 +0300

    Windows 1/2

--- Build.bat
+++ Build.bat
@@ -2,10 +2,10 @@
 
 cls
 
-cmake -Wno-dev zydis -B zydis
+cmake -D CMAKE_C_COMPILER=i686-w64-mingw32-gcc -D CMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -G "MinGW Makefiles" -Wno-dev zydis -B zydis
 
-cmake -Wno-dev zydis\dependencies\zycore -B zydis\dependencies\zycore
+cmake -D CMAKE_C_COMPILER=i686-w64-mingw32-gcc -D CMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -G "MinGW Makefiles" -Wno-dev zydis\dependencies\zycore -B zydis\dependencies\zycore
 
-cmake -Wno-dev -B CMake
+cmake -D CMAKE_C_COMPILER=i686-w64-mingw32-gcc -D CMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -G "MinGW Makefiles" -Wno-dev -B CMake
 
 make -C CMake -s
--- Build.sh
+++ Build.sh
@@ -2,10 +2,10 @@
 
 clear
 
-cmake -Wno-dev zydis -B zydis
+cmake -D CMAKE_C_COMPILER=i686-w64-mingw32-gcc -D CMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -Wno-dev zydis -B zydis
 
-cmake -Wno-dev zydis/dependencies/zycore -B zydis/dependencies/zycore
+cmake -D CMAKE_C_COMPILER=i686-w64-mingw32-gcc -D CMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -Wno-dev zydis/dependencies/zycore -B zydis/dependencies/zycore
 
-cmake -Wno-dev -B CMake
+cmake -D CMAKE_C_COMPILER=i686-w64-mingw32-gcc -D CMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -Wno-dev -B CMake
 
 make -C CMake -s
