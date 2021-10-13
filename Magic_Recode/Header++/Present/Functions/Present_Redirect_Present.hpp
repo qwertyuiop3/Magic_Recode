19ed062285d6c66eb1c562aaf91261bfe3e3adc8
qwertyuiop3 <43570227+qwertyuiop3@users.noreply.github.com>
2021-10-13 23:49:16 +0300

    Commit

--- Magic_Recode/Header++/Present/Functions/Present_Redirect_Present.hpp
+++ Magic_Recode/Header++/Present/Functions/Present_Redirect_Present.hpp
@@ -12,7 +12,7 @@ void Redirect_Present(IDirect3DDevice9* Direct_3_Dimensional_Device_9)
 	{
 		if (*(unsigned __int8*)Present_Location == 233)
 		{
-			Present_Location = (void*)((unsigned __int32)Present_Location + *(unsigned __int32*)((unsigned __int32)Present_Location + 1) + 5);
+			Present_Location = (void*)((unsigned __int32)Present_Location + *(__int32*)((unsigned __int32)Present_Location + 1) + 5);
 
 			goto Resolve_Present_Location_Label;
 		}
